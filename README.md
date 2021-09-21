ef6381e5cfbbc60ee9f6a25d7fe5e4415c6fe56e
Tyler Davis <tylerrusselldavis@gmail.com>
2021-09-21 18:50:51 -0500

    Created README

---
+++ README.md
@@ -0,0 +1,4 @@
+# PixelFace
+## Goal
+1. To create a Python app that will take a photo of a face as an input, and output a pixel art version of that photo.
+2. To create a web interface for that application.
--- colorPalette.py
+++ colorPalette.py
@@ -1,3 +1,12 @@
 def returnRGB(colors):
     rgb_list = [x[0] for x in colors]
     return rgb_list
+
+
+
+
+
+#comparing colors
+#convert to HSL?
+#compare in stages? is red similar, is green similar, is blue similar
+#break colors down into groupings, compare within those?
