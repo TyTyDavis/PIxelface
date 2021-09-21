abfbfa10fc3573ef8eff625b154ab05a028c6da5
Tyler Davis <tylerrusselldavis@gmail.com>
2021-09-21 15:37:26 -0500

    Started process of extracting colors for palette

---
+++ colorPalette.py
@@ -0,0 +1,3 @@
+def returnRGB(colors):
+    rgb_list = [x[0] for x in colors]
+    return rgb_list
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib
 import numpy
 import extcolors
 import resize
+import colorPalette
 
 import PIL
 from PIL import Image
@@ -14,10 +15,21 @@ img = Image.open(os.path.join(ROOT_DIR, "Portrait.jpg"))
 img = img.rotate(-90, expand=True)
 #Change image size
 img = resize.pixelResize(img, 100, 200)
+colors, pixelCount = extcolors.extract_from_image(img)
+
+img.save(os.path.join(ROOT_DIR, "Portrait.png"))
 
 #Change color pallete
+colors = colorPalette.returnRGB(colors)
 
-img.save(os.path.join(ROOT_DIR, "newPortrait.png"))
+newPixels = []
+for color in img.getdata():
+    newPixels.append(colors[0])
 
-#img.show()
+newImg = Image.new(img.mode,img.size)
+newImg.putdata(newPixels)
+
+
+
+newImg.show()
 img.close()
